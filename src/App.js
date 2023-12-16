86126204353ec217b2b20e68bd85e38c2de12fc4
gautambhardwaj-966 <gautambhardwaj9661@gmail.com>
2023-12-17 00:44:28 +0530

    Till Logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './index.css';
+import Head from './components/Head';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-green-900 text-3xl font-bold">
-      This is Youtube clone
+    <div >
+   <h1 className="text-green-900 text-3xl font-bold">This is Youtube clone</h1>
+      
+      <Head/>
+      <Body/>
     </div>
   );
 }
