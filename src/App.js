2ca526dd8486d7e0aeda82d1151f0f62d03b6e5a
gautambhardwaj-966 <gautambhardwaj9661@gmail.com>
2023-12-18 14:41:42 +0530

    Till Head

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import Body from './components/Body';
 function App() {
   return (
     <div >
-   <h1 className="text-green-900 text-3xl font-bold">This is Youtube clone</h1>
-      
+  
       <Head/>
       <Body/>
     </div>
