2c6864cc51bc06bb7a1de26b0d5e455c2701c091
gautambhardwaj-966 <gautambhardwaj9661@gmail.com>
2023-12-25 23:55:55 +0530

    Comments

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux';
 import { closeMenu } from '../utils/appSlice';
 import { useParams ,useSearchParams } from 'react-router-dom';
+import CommentsContainer from './CommentsContainer';
 
 const WatchPage = () => {
  //This will console empty objects because / is not there   
@@ -15,12 +16,15 @@ dispatch(closeMenu());
     },[]);
 
   return (
+    <div className='flex flex-col'>
     <div className='px-5'>
       <iframe width="1200" 
       height="600" 
       src={"https://www.youtube.com/embed/"+ searchParams.get("v")}title="YouTube video player"  
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
       allowfullscreen></iframe>
+    </div>
+    <CommentsContainer/>
     </div>
   )
 };
